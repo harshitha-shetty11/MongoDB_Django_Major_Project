512057a0057fb9db6f9a82780ab8659bb36b6607
harshitha-shetty11 <harshushetty1206@gmail.com>
2025-07-23 21:24:23 +0530

    Delete data_app directory

--- data_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.list_people, name='list_people'),  # Root URL now shows the list
-    path('add/', views.add_person, name='add_person'),
-    path('list/', views.list_people, name='list_people')  # Keep this for backward compatibility
-]
